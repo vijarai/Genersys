2172d2083049cc8cf5e23cfec6d23bd68df27f22
vijarai <vijayaprasada.rai@itv.com>
2015-06-10 14:59:49 +0100

    ssss

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world.!!!");
+            Console.ReadLine();
         }
     }
 }
